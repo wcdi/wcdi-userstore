3cd937bf27a4ead1df3d04ff651ca1b179504d4a
Naoki Kanazawa <kanazawa@naoki912.net>
2017-11-19 13:32:56 +0900

    [Change] main.go: panicからFprintfに変更

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"os"
+	"fmt"
+
 	"github.com/wcdi/wcdi-userstore/cmd"
 )
 
 func main() {
 	if err := cmd.RootCmd.Execute(); err != nil {
-		panic(err)
+		fmt.Fprintf(os.Stderr, "Error: %s\n", err)
 	}
 }
