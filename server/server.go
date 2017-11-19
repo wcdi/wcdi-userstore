fcdd0188bfffd1ef0b808b43f65d385fac3dd4fe
Naoki Kanazawa <kanazawa@naoki912.net>
2017-11-19 13:21:00 +0900

    [Add] server.go: negroniのLoggerを追加した

--- server/server.go
+++ server/server.go
@@ -3,10 +3,16 @@ package server
 import (
 	"log"
 	"net/http"
+
+	"github.com/urfave/negroni"
 )
 
 func StartServer() {
 	router := NewRouter()
 
-	log.Fatal(http.ListenAndServe(":8080", router))
+	n := negroni.New()
+	n.Use(negroni.NewLogger())
+	n.UseHandler(router)
+
+	log.Fatal(http.ListenAndServe(":8080", n))
 }
